77bcfa5499d8352003034cb7bfefbae3b5005236
RSCSAP <richard.s.wvhs@gmail.com>
2024-03-19 09:58:34 -0700

    i

--- app/src/main/java/com/example/kitchensync/MainActivity.kt
+++ app/src/main/java/com/example/kitchensync/MainActivity.kt
@@ -7,16 +7,18 @@ import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.LinearSnapHelper
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.SnapHelper
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     private lateinit var drawerLayout: DrawerLayout
-
+    private lateinit var bottomNavigationView: BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -41,6 +43,10 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
         //use this link later for image searching
         //https://google.com/search?q=space&tbm=isch
+        bottomNavigationView = findViewById(R.id.bottom_navigation)
+    }
+    private fun replaceFragment (fragment : Fragment){
+        supportFragmentManager.beginTransaction().replace(R.id.fragment_container, fragment).commit()
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
