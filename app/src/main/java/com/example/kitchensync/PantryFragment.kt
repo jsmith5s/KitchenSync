aba4fe7feebed16953c0402111270c2524daa61d
rblos <jordan@mastahype.com>
2024-03-20 15:43:01 -0700

    skrape it incorporation

--- app/src/main/java/com/example/kitchensync/PantryFragment.kt
+++ app/src/main/java/com/example/kitchensync/PantryFragment.kt
@@ -11,12 +11,14 @@ import it.skrape.fetcher.extractIt
 import it.skrape.fetcher.skrape
 import it.skrape.selects.html5.*
 
+data class Country(var name: String="", val population: String="")
+
+data class ScrapingResult(val countries: MutableList<Country> = mutableListOf(), var count:Int = 0)
+
 class PantryFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-
 
     }
 
@@ -28,21 +30,48 @@ class PantryFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_pantry, container, false)
     }
 
-    /*fun main(){
+    fun main(){
         // Set your website URL
         val website_url = "https://en.wikipedia.org/wiki/List_of_countries_and_dependencies_by_population"
-        val countries : List<String> = skrape(HttpFetcher) {
+        val countries = skrape(HttpFetcher) {
             request {
                 // Tell skrape{it} which URL to fetch data from
                 url = website_url
             }
 
 
-            extractIt<ScrapingResult> { results ->
+            extractIt<ScrapingResult> {results ->
                 htmlDocument{
                     // Main function where you'll parse web data
+                    val countryRows = table(".wikitable") {
+                        tr{
+                            findAll{this}
+                        }
+                    }
+
+                    countryRows
+                        .drop(2)  // Remove the first two elements; these are just the table header and subheader
+                        .map{
+                            // Define variables to hold name and population
+                            var name: String =""
+                            var population: String=""
+                            it.a{
+                                findFirst(){   // Find the first <a> tag
+                                    name = text    // Extract its text (this is the name of the country)
+                                    println("Name - $text ")
+                                }
+                            }
+                            it.td{
+                                findSecond(){    // Find the second <td> tag
+                                    population = text   // Extract its text (this is the population of the country)
+                                    println("Population - $text \n")
+                                }
+                            }
+                            results.countries.add(Country(name,population))   // Create a country and add it to the results object
+                            results.count = results.countries.size  // Get the number of countries and add it to the results object
+                        }
                 }
             }
         }
-    }*/
+    }
 }
